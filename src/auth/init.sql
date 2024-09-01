60e1ab9f552b4d2a847f84fbbdc03d26e2d8e04c
syafiqahlukman <ssyafiqahlukman@gmail.com>
2024-09-01 15:39:45 +0800

    setup db

--- src/auth/init.sql
+++ src/auth/init.sql
@@ -1,20 +1,20 @@
-'''
+/*
 setup database environment
 this file does not require configuration as it is a script executed directly on the database.
-'''
+*/
 
--- create new db user 
+-- create new db user account in MySQL 
 -- allows the db user to access the auth db
 -- used by application to connect to the db
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123'; 
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
 
---creates a new db named auth
+-- creates a new db named auth
 CREATE DATABASE auth;
 
 -- grants the db user (auth_user) all privileges on the auth db. auth.* refers to all tables in the db
 GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 
---use auth db for susequent commands
+-- use auth db for subsequent commands
 USE auth;
 
 -- creates a table named user, with columns id, email, and password
@@ -28,7 +28,7 @@ CREATE TABLE user (
 );
 
 -- add a new entry (user) to the user table
-INSERT INTO user (email, password) VALUES ('georgio@email.com', 'Admin123');
+INSERT INTO user (email, password) VALUES ('ssyafiqahlukman@gmail.com', 'Admin123');
 
   
 
