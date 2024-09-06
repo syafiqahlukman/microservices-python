f010fc3b2988983ef2bf8b9db9ac7d3291cb57ab
nur syafiqah <ssyafiqahlukman@gmail.com>
2024-09-06 15:58:50 +0800

    add server logic for gateway service

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -1,27 +1,27 @@
-import os, gridfs, pika, json
-from flask import Flask, request, send_file
-from flask_pymongo import PyMongo
-from auth import validate
-from auth_svc import access
-from storage import util
-from bson.objectid import ObjectId
+import os, gridfs, pika, json #os to interact with the os, gridfs to store large files in mongodb, pika is python lib to interact with rabbitmq, json to work with json data (used for API responses and requests)
+from flask import Flask, request, send_file #flask is the web framework and defines route for the app, request is flask module to handle HTTP request, send_file to send files to the client (the converted mp3 file)
+from flask_pymongo import PyMongo #flask_pymongo is a flask extension to interact with mongodb from within the Flask app
+from auth import validate #auth is the module to validate the token
+from auth_svc import access #auth_svc for handling authentication services
+from storage import util #storage is the module to handle file storage
+from bson.objectid import ObjectId #bson is a module to work with ObjectId which is unique identifier for documents in MongoDB
 
-server = Flask(__name__)
+server = Flask(__name__) #Initializes a Flask application
 
-mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos") #connects to the mongodb server for storing videos
 
-mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s") #connects to the mongodb server for storing mp3s
 
-fs_videos = gridfs.GridFS(mongo_video.db)
-fs_mp3s = gridfs.GridFS(mongo_mp3.db)
+fs_videos = gridfs.GridFS(mongo_video.db) #Initializes GridFS for storing large video files.
+fs_mp3s = gridfs.GridFS(mongo_mp3.db) #Initializes GridFS for storing large mp3 files.
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
-channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq")) #connects to the rabbitmq server
+channel = connection.channel() #creates a channel to interact with the rabbitmq server (send and receive messages)
 
 
-@server.route("/login", methods=["POST"])
+@server.route("/login", methods=["POST"]) #routes for user login
 def login():
-    token, err = access.login(request)
+    token, err = access.login(request) #calls the login function from auth_svc module
 
     if not err:
         return token
