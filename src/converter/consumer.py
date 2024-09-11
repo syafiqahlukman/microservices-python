e00858aec88143f85c8ed34c9c24be5a9f014bef
nur syafiqah <ssyafiqahlukman@gmail.com>
2024-09-11 21:52:40 +0800

    add consumer service

--- src/converter/consumer.py
+++ src/converter/consumer.py
@@ -1,38 +1,44 @@
 import pika, sys, os, time
 from pymongo import MongoClient
 import gridfs
-from convert import to_mp3
+from convert import to_mp3 #package that we are going to create to convert video to mp3
 
 
 def main():
-    client = MongoClient("host.minikube.internal", 27017)
-    db_videos = client.videos
-    db_mp3s = client.mp3s
+    client = MongoClient("host.minikube.internal", 27017) 
+    #MongoDB server runs on local machine, and the consumer service connects to it using the hostname host.minikube.internal.
+    db_videos = client.videos #to interact with videos db using db_videos variable 
+    db_mp3s = client.mp3s ##to interact with mp3s db using db_mp3s variable 
+    #so these dbs will exist within out mongodb
+
     # gridfs
     fs_videos = gridfs.GridFS(db_videos)
     fs_mp3s = gridfs.GridFS(db_mp3s)
 
     # rabbitmq connection
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+    # RabbitMQ server runs within the Kubernetes cluster, and the consumer service connects to it using the hostname rabbitmq.
     channel = connection.channel()
 
-    def callback(ch, method, properties, body):
-        err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
+    def callback(ch, method, properties, body): 
+    #callback function is executed whenever a message is pulled from the RabbitMQ queue. It processes the message by converting the video to MP3 (calling the to_mp3.start function)and handles acknowledgment (basic_ack and basic_nack)based on whether the conversion was successful.
+        err = to_mp3.start(body, fs_videos, fs_mp3s, ch) #when we get the message is we want to convert the video to MP3
         if err:
             ch.basic_nack(delivery_tag=method.delivery_tag)
         else:
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
+    #configuration to consume messages from the VIDEO_QUEUE. The queue name is retrieved from an environment variable.
     channel.basic_consume(
-        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
+        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback #The consumer service starts listening for messages and processes them using the callback function. 
     )
 
-    print("Waiting for messages. To exit press CTRL+C")
+    print("Waiting for messages. To exit press CTRL+C") #starts the consumer, which continuously listen for messages on the queue and process using callback function
 
     channel.start_consuming()
 
-if __name__ == "__main__":
-    try:
+if __name__ == "__main__": 
+    try: #consumer service runs until interrupted by the user. When interrupted, it gracefully shuts down the service.
         main()
     except KeyboardInterrupt:
         print("Interrupted")
@@ -40,3 +46,6 @@ if __name__ == "__main__":
             sys.exit(0)
         except SystemExit:
             os._exit(0)
+
+#this file contains core logic for consumer service, which processes messages from a RabbitMQ queue, converts video files to mp3 and interacts with mongodb
+#p/s run mongodb on local to save resources within kubernetes cluster
--- src/converter/convert/to_mp3.py
+++ src/converter/convert/to_mp3.py
@@ -1,35 +1,35 @@
 import pika, json, tempfile, os
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId # for working with MongoDB in Python.
 import moviepy.editor
 
 
 def start(message, fs_videos, fs_mp3s, channel):
-    message = json.loads(message)
+    message = json.loads(message) #The message is being converted from JSON format to langauge that can be understand by the program
 
-    # empty temp file
+    # add empty temp file to hold the video contents
     tf = tempfile.NamedTemporaryFile()
-    # video contents
-    out = fs_videos.get(ObjectId(message["video_fid"]))
-    # add video contents to empty file
-    tf.write(out.read())
-    # create audio from temp video file
-    audio = moviepy.editor.VideoFileClip(tf.name).audio
-    tf.close()
+   
+    out = fs_videos.get(ObjectId(message["video_fid"])) #video file is retrieved from the fs_videos collection using its ObjectId.
+    
+    tf.write(out.read()) #The video contents are written to the temporary file.
+   
+    audio = moviepy.editor.VideoFileClip(tf.name).audio #The audio is extracted from the video file using moviepy.editor.
+    tf.close() #twmp file is closed
 
-    # write audio to the file
+    # write audio to a new temp file
     tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
     audio.write_audiofile(tf_path)
 
-    # save file to mongo
+    # The MP3 file is read and saved to the fs_mp3s collection in MongoDB.
     f = open(tf_path, "rb")
     data = f.read()
     fid = fs_mp3s.put(data)
     f.close()
-    os.remove(tf_path)
+    os.remove(tf_path) #the temp mp3 file is removed
 
-    message["mp3_fid"] = str(fid)
+    message["mp3_fid"] = str(fid) #The file ID (mp3_fid) is added to the message.
 
-    try:
+    try: #The message is published to the MP3_QUEUE in RabbitMQ.
         channel.basic_publish(
             exchange="",
             routing_key=os.environ.get("MP3_QUEUE"),
@@ -38,6 +38,8 @@ def start(message, fs_videos, fs_mp3s, channel):
                 delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             ),
         )
-    except Exception as err:
+    except Exception as err: #If the message fails to publish, the file is deleted from the fs_mp3s collection in mongodb
         fs_mp3s.delete(fid)
         return "failed to publish message"
+
+#this to_mp3.py is a function that processes a video file, extracts its audio, converts to mp3 format, and then saves it to mongodb 
